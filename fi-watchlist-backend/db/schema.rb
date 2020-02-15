e626a65ffa0e52a9c39b5d9809ef5bdf1934432c
rayvaldez <ray_valdez@hotmail.co.uk>
2020-02-15 10:40:05 +0000

    Migrate and seed database

---
+++ fi-watchlist-backend/db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_15_000534) do
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.string "team"
+    t.decimal "cost", precision: 8, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "watchlist_players", force: :cascade do |t|
+    t.decimal "cost", precision: 8, scale: 2
+    t.integer "player_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_id"], name: "index_watchlist_players_on_player_id"
+  end
+
+  add_foreign_key "watchlist_players", "players"
+end
