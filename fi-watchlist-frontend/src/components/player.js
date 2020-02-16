7cabd9956d83e656eae4173de00cc8ff96c19004
rayvaldez <ray_valdez@hotmail.co.uk>
2020-02-16 11:22:21 +0000

    Add Players to Index view

---
+++ fi-watchlist-frontend/src/components/player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor(playerJSON) {
+    this.id = playerJSON.id
+    this.name = playerJSON.name
+    this.team = playerJSON.team
+    this.cost = playerJSON.cost
+  }
+
+  renderTd() {
+    return `
+            <tr>
+              <td><a href="http://localhost:3000/api/v1/players/${this.id}" data-id="${this.id}" class="index-player" data-toggle="modal" data-target="#playerModal">${this.name}</a></td>
+              <td>${this.team}</td>
+              <td>£${this.cost}</td>
+            </tr>
+          `
+  }
+
+}
--- fi-watchlist-frontend/src/components/players.js
+++ fi-watchlist-frontend/src/components/players.js
@@ -3,9 +3,29 @@ class Players {
     this.players = []
     this.player = {}
     this.adapter = new PlayersAdapter()
-    this.initBindingsAndEventListeners()
+    this.initiBindingsAndEventListeners()
     this.fetchAndLoadPlayers()
   }
 
-  
+  initiBindingsAndEventListeners() {
+    this.playersTable = document.getElementById('players-table')
+  }
+
+  fetchAndLoadPlayers() {
+    this.adapter
+    .getPlayers()
+    .then(players => {
+      players.forEach(player => this.players.push(new Player(player)))
+    })
+    .then(() => {
+      this.players.sort((a, b) => parseFloat(b.cost) - parseFloat(a.cost))
+    })
+    .then(() => {
+      this.renderPlayers()
+    })
+  }
+
+  renderPlayers() {
+    document.getElementById('players-table').getElementsByTagName('tbody')[0].innerHTML = this.players.map(player => player.renderTd()).join('')
+  }
 }
