85e1913f797dce5e99841c04082a23a2a978172d
rayvaldez <ray_valdez@hotmail.co.uk>
2020-02-17 23:34:39 +0000

    Add Player modal added to Index view

--- fi-watchlist-frontend/src/components/watchlist_players.js
+++ fi-watchlist-frontend/src/components/watchlist_players.js
@@ -17,5 +17,16 @@ class WatchlistPlayers {
 
   fetchAndLoadPlayer(e) {
     e.preventDefault()
+    return fetch(`${e.target}`)
+    .then(res => res.json())
+    .then(json => {
+      document.getElementById('player-show-name').innerHTML = `<h4>Add ${json.name} to Watchlist?</h4>`
+      document.getElementById('player-show-info').innerHTML = `
+      <input type="hidden" id="player-id" value="${json.id}" />
+      <input type="hidden" id="player-cost" value="${json.cost}" />
+      <h6>Team - ${json.team}</h6> <br />
+      <h6>Cost - £${json.cost}</h6>
+      `
+    })
   }
 }
