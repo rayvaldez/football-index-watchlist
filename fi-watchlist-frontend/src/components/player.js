c2ec0fdeace75086b34e184953f354867acc71b6
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-30 18:23:56 +0000

    Refactor players.js into player.js

--- fi-watchlist-frontend/src/components/player.js
+++ fi-watchlist-frontend/src/components/player.js
@@ -6,16 +6,38 @@ class Player {
     this.cost = playerJSON.cost;
   }
 
+  static fetchAndLoadPlayers() {
+    const playerAdapter = new PlayersAdapter();
+    playerAdapter.getPlayers()
+    .then(players => {
+      players.forEach(player => this.all.push(new Player(player)));
+    })
+    .then(() => {
+      this.all.sort((a, b) => parseFloat(b.cost) - parseFloat(a.cost));
+    })
+    .then(() => {
+      this.renderPlayers();
+    });
+  }
+
+  static renderPlayers() {
+    Player.table.innerHTML = this.all.map(player => player.renderTd()).join('');
+  }
+
   renderTd() {
     return `
-            <tr>
-              <td><a href="http://localhost:3000/api/v1/players/${this.id}"
-                data-id="${this.id}" class="index-player" data-toggle="modal"
-                data-target="#playerModal">${this.name}</a></td>
-              <td>${this.team}</td>
-              <td>£${this.cost}</td>
-            </tr>
+      <tr>
+        <td><a href="http://localhost:3000/api/v1/players/${this.id}"
+          data-id="${this.id}" class="index-player" data-toggle="modal"
+          data-target="#playerModal">${this.name}</a></td>
+        <td>${this.team}</td>
+        <td>£${this.cost}</td>
+      </tr>
           `;
   }
 
 }
+
+Player.all = [];
+
+Player.table = document.getElementsByTagName('tbody')[1];
